5a1c432ba3152c39637f58976373d5b93e42f367
Eugênio Cabral <eugfcl@gmail.com>
2016-02-20 23:39:02 -0200

    Initial version

---
+++ video_embed.rb
@@ -0,0 +1,37 @@
+class VideoEmbed < Liquid::Tag
+
+  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+
+  Hosts = {
+    "ted" => lambda { |id| "https://embed-ssl.ted.com/talks/#{id}.html" },
+    "ustream" => lambda { |id| "http://www.ustream.tv/embed/#{id}?html5ui" },
+    "vimeo" => lambda { |id| "https://player.vimeo.com/video/#{id}" },
+    "youtube" => lambda { |id| "http://www.youtube.com/embed/#{id}?color=white&theme=light" }
+  }
+
+  def initialize(tag_name, markup, tokens)
+    super
+
+    if markup =~ Syntax then
+      @host = Hosts[tag_name]
+      @id = $1
+
+      if $2.nil? then
+          @width = 640
+          @height = 360
+      else
+          @width = $2.to_i
+          @height = $3.to_i
+      end
+    else
+      raise "No video ID provided in the \"#{tag_name}\" tag"
+    end
+  end
+
+  def render(context)
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@host.call(@id)}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+  end
+  
+  Hosts.each_key {|key| Liquid::Template.register_tag key, self}
+
+end
