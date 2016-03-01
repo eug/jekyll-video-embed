3b19a6f65fbc292c355977080a7c810f7f766c76
Eugênio Cabral <eugfcl@gmail.com>
2016-03-01 17:03:59 -0300

    Add Jekyll module

--- video_embed.rb
+++ video_embed.rb
@@ -1,37 +1,40 @@
-class VideoEmbed < Liquid::Tag
 
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+module Jekyll
+  class VideoEmbed < Liquid::Tag
 
-  Hosts = {
-    "ted"     => ->(id) { "https://embed-ssl.ted.com/talks/#{id}.html" },
-    "ustream" => ->(id) { "http://ustream.tv/embed/#{id}" },
-    "vimeo"   => ->(id) { "https://player.vimeo.com/video/#{id}" },
-    "youtube" => ->(id) { "http://youtube.com/embed/#{id}" }
-  }
+    Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
-  def initialize(tag_name, markup, tokens)
-    super
+    Hosts = {
+      "ted"     => ->(id) { "https://embed-ssl.ted.com/talks/#{id}.html" },
+      "ustream" => ->(id) { "http://ustream.tv/embed/#{id}" },
+      "vimeo"   => ->(id) { "https://player.vimeo.com/video/#{id}" },
+      "youtube" => ->(id) { "http://youtube.com/embed/#{id}" }
+    }
 
-    if markup =~ Syntax then
-      @host = Hosts[tag_name]
-      @id = $1
+    def initialize(tag_name, markup, tokens)
+      super
 
-      if $2.nil? then
-          @width = 640
-          @height = 360
+      if markup =~ Syntax then
+        @host = Hosts[tag_name]
+        @id = $1
+
+        if $2.nil? then
+            @width = 640
+            @height = 360
+        else
+            @width = $2.to_i
+            @height = $3.to_i
+        end
       else
-          @width = $2.to_i
-          @height = $3.to_i
+        raise "No video ID provided in the \"#{tag_name}\" tag"
       end
-    else
-      raise "No video ID provided in the \"#{tag_name}\" tag"
     end
-  end
 
-  def render(context)
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@host.call(@id)}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
-  end
-  
-  Hosts.each_key { |key| Liquid::Template.register_tag key, self }
+    def render(context)
+      "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@host.call(@id)}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+    end
+    
+    Hosts.each_key { |key| Liquid::Template.register_tag key, self }
 
+  end
 end
